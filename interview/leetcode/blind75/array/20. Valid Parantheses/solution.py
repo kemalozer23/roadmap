96be0c6d302e977890f3df794180e20575f636a6
kemalozer23 <kemalozer23@gmail.com>
2025-04-04 01:53:55 +0300

    p

--- interview/leetcode/blind75/array/20. Valid Parantheses/solution.py
+++ interview/leetcode/blind75/array/20. Valid Parantheses/solution.py
@@ -1,5 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        if len(s) == 1:
+            return False
+
         stack = []
         hashMap = {')': '(', '}': '{', ']': '['}
 
@@ -7,9 +12,11 @@ class Solution:
             if p not in hashMap: # its an opening bracket
                 stack.append(p)
             else: # its a closing bracket
-                if stack.count == 0:
+                if len(stack) == 0:
                     return False
                 elif hashMap[p] == stack[-1]:
                     stack.pop()
+                else:
+                    return False
 
         return True if not stack else False
